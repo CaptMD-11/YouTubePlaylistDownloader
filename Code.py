b89471b6f520ae4f853a7018bbac0e0ccf727c32
CaptMD-11 <86084719+CaptMD-11@users.noreply.github.com>
2023-06-07 23:35:08 -0700

    changed some naming

--- Code.py
+++ Code.py
@@ -1,18 +1,23 @@
 import subprocess
 import os
 
-user_input = input("enter link here: ")
+user_input = input("Enter link here: ")
 
 # Command to execute
 command = 'yt-dlp -x ' + user_input + ' --audio-format \"mp3\"'
 
-path = "~/git/YouTubeDownloader/test"
-working_directory = os.path.expanduser(path)
-os.mkdir(working_directory)
+path = "~/git/YouTubePlaylistDownloader/ExportedFiles"
+exported_directory = os.path.expanduser(path)
+os.mkdir(exported_directory)
+
+urls = ['https://youtu.be/oiWWKumrLH8', 'https://youtu.be/tPEE9ZwTmy0']
 
 # Execute the command
+
+# for i in urls:
 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE, cwd=working_directory)
+                           stderr=subprocess.PIPE, cwd=exported_directory)
+
 
 # Wait for the command to finish and get the output and errors
 output, errors = process.communicate()
